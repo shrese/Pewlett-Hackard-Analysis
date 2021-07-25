ef6bb63a58180643f4a7b975eab7742a06ce14e0
shrese <srudd1@hotmail.com>
2021-07-25 07:29:49 -0600

    added imported tables

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/schema.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/schema.sql
@@ -35,23 +35,20 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE emp_depts (
+CREATE TABLE emp_dept (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR (40) NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	to_date DATE NOT NULL
 );
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR (40) NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	to_date DATE NOT NULL
+--FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+--	PRIMARY KEY (emp_no)
 );
 
 SELECT * from
